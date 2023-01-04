5fcf36a21073d5048f3d04464b1934812de00c8c
unknown <dainius.skrivelis@gmail.com>
2023-01-04 10:28:27 +0200

    Adding boilerplate code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Users from "./users/pages/Users";
+
+function App() {
+  return (
+    <Router>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/users/pages/Users.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Users() {
+  return <h1>Users works!</h1>;
+}
+export default Users;
